61275618604e7dabc2a082e31f3965c855c66565
reyDEsol <ku.k669500@gmail.com>
2018-09-27 21:34:23 +0900

    Optimization

--- app/src/main/java/com/example/sunsh/sainttropez/MainActivity.kt
+++ app/src/main/java/com/example/sunsh/sainttropez/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.sunsh.sainttropez
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.ContextMenu
@@ -9,6 +7,10 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.email
+import org.jetbrains.anko.sendSMS
+import org.jetbrains.anko.share
 
 class MainActivity : AppCompatActivity() {
 
@@ -55,36 +57,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onContextItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.sms -> {
-                val number = "999-9999-9999"
-                val uri = Uri.parse("sms:$number")
-                var intent = Intent(Intent.ACTION_VIEW)
-                intent.apply {
-                    data = uri
-                    putExtra("sms_body", "予約をしたいです")
-                }
+        return when (item?.itemId) {
+            R.id.sms -> sendSMS("999-9999-9999", "予約をしたいです")
+            /*{
+            val number = "999-9999-9999"
+            val uri = Uri.parse("sms:$number")
+            var intent = Intent(Intent.ACTION_VIEW)
+            intent.apply {
+                data = uri
+                putExtra("sms_body", "予約をしたいです")
+            }
+            startActivity(intent)
+            return true
+        }*/
+            R.id.mail -> email("nobody@example.com", "予約の問い合わせ",
+                    "以下の通り予約希望します。")
+            /*{
+            val email = "nobody@example.com"
+            val subject = "予約の問い合わせ"
+            val text = "以下の通り予約希望します。"
+            val uri = Uri.parse("mailto:")
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.apply {
+                data = uri
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, text)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
                 startActivity(intent)
-                return true
             }
-            R.id.mail -> {
-                val email = "nobody@example.com"
-                val subject = "予約の問い合わせ"
-                val text = "以下の通り予約希望します。"
-                val uri = Uri.parse("mailto:")
-                val intent = Intent(Intent.ACTION_SENDTO)
-                intent.apply {
-                    data = uri
-                    putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-                    putExtra(Intent.EXTRA_SUBJECT, subject)
-                    putExtra(Intent.EXTRA_TEXT, text)
-                }
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivity(intent)
-                }
-                return true
-            }
-            R.id.share -> {
+            return true
+        }*/
+            R.id.share -> share("美味しいレストランを紹介します。")
+            /*{
                 val text = "美味しいレストランを紹介します。"
                 val intent = Intent(Intent.ACTION_SEND)
                 intent.apply {
@@ -96,8 +102,9 @@ class MainActivity : AppCompatActivity() {
                     startActivity(chooser)
                 }
                 return true
-            }
-            R.id.brows -> {
+            }*/
+            R.id.brows -> browse("https://google.com")
+            /*{
                 val url = "https://google.com"
                 val intent = Intent(Intent.ACTION_VIEW)
                 intent.data = Uri.parse(url)
@@ -105,8 +112,8 @@ class MainActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
                 return true
-            }
+            }*/
+            else -> super.onContextItemSelected(item)
         }
-        return super.onContextItemSelected(item)
     }
 }
